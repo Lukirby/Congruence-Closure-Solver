0492102262612df9db6de7ad25df81baf002984d
Lukirby <luca.panariello2@studio.unibo.it>
2024-12-20 11:40:12 +0100

    added array signature

---
+++ project/preprocessing/ArraySignature.java
@@ -0,0 +1,11 @@
+package project.preprocessing;
+
+public class ArraySignature {
+    
+    public static String select = "select";
+    
+    public static String store = "store";
+
+    public static String selectOverStore = select+"\\("+store;
+
+}
